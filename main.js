8f30374bd631ae76995459f3a2a496adfe5da797
Sher06777 <asherbolot1@gmail.com>
2023-05-27 17:18:12 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+let users = [
+    {id: 1, name: "Вася"},
+    {id: 2, name: "Петя"},
+    {id: 3, name: "Маша"}
+];
+
+users.forEach(user => user.isAdmin = false);
+
+console.log(users);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(users.filter((item, idx)=>{
+
+// }));
